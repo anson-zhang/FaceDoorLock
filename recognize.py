dcb0f5505e2006caab8cbf70d43c26932f49e706
anson-zhang <N@Zhuoqi-MacBook-Pro.local>
2017-04-18 21:00:28 -0400

    Face Door Lock 3.1
    Face Door Lock 3.1

--- recognize.py
+++ recognize.py
@@ -15,15 +15,9 @@ dic = dict()
 for l in open("./Faces/face_label.txt"):
 	dic[l.split(" ")[0]] = l.split(" ")[1].strip("\n")
 
-t_start = time.time()
-fps = 0
-while True:
-	ret, frame = video_capture.read()
-	fps = fps + 1
-	sfps = fps / (time.time() - t_start)
-	cv2.putText(frame, "FPS : " + str(sfps), (0, 480), cv2.FONT_HERSHEY_SIMPLEX, 1, ( 0, 255, 0 ))
-	cv2.imshow("Face Door Lock", frame)
-	if cv2.waitKey(5) & 0xFF == ord('t'):
+#Recognize
+def rec(event, x, y, flags, frame):
+	if event == cv2.EVENT_RBUTTONDOWN:
 		#Defind face recognizer: LBPH
 		recognizer = cv2.createLBPHFaceRecognizer()
 		recognizer.load("fdl.xml")
@@ -32,22 +26,35 @@ while True:
 		faces = faceCascade.detectMultiScale(image, 1.2)
 		for (x, y, w, h) in faces:
 			label_predict, conf = recognizer.predict(image[y: y + h, x: x + w])
-			if conf < 50.0:
+			if conf < 65.0:
 				cv2.rectangle(col_img, (x, y), (x + w, y + h), (0, 255, 0), 2)
-				cv2.putText(col_img, "Welcome " + str(dic[str(label_predict)]) + "!", (0, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255 ,0))
+				cv2.rectangle(col_img, (0,420), (640,460), (255, 255, 255), -1)
+				cv2.putText(col_img, "Welcome " + str(dic[str(label_predict)]) + "!", (0, 450), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0 ,0))
 				cv2.putText(col_img, "Open for " + str(dic[str(label_predict)]) + " " + str(conf), (x, y + h), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255 ,0))
 				cv2.imwrite("./visitors/" + dic[str(label_predict)] + str(time.strftime("-%Y-%m-%d-%H_%M_%S", time.localtime())) + ".jpg", col_img)
 				cv2.imshow("Face Door Lock", col_img)
 				cv2.waitKey(3000)
 			else:
-				if conf < 100.0:
+				if conf < 300.0:
 					cv2.rectangle(col_img, (x, y), (x + w, y + h), (0, 255, 0), 2)
-					cv2.putText(col_img, "Sorry! You are not allowed to come in.", (0, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255 ,0))
+					cv2.rectangle(col_img, (0,420), (640,460), (255, 255, 255), -1)
+					cv2.putText(col_img, "Sorry! You are not allowed to come in.", (0, 450), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 0))
 					cv2.putText(col_img, "unknown " + str(dic[str(label_predict)]) + str(conf), (x, y + h), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255 ,0))
 					cv2.imwrite("./visitors/" + "unknown" + str(time.strftime("-%Y-%m-%d-%H_%M_%S", time.localtime())) + ".jpg", col_img)
 					cv2.imshow("Face Door Lock", col_img)
-					cv2.waitKey(1000)
+					cv2.waitKey(1500)
 
+t_start = time.time()
+fps = 0
+cv2.namedWindow('Face Door Lock')
+while True:
+	ret, frame = video_capture.read()
+	cv2.setMouseCallback('Face Door Lock', rec, frame)
+	#fps = fps + 1
+	#sfps = fps / (time.time() - t_start)
+	#cv2.putText(frame, "FPS : " + str(sfps), (0, 480), cv2.FONT_HERSHEY_SIMPLEX, 1, ( 0, 255, 0 ))
+	cv2.imshow("Face Door Lock", frame)
+	#if cv2.waitKey(5) & 0xFF == ord('t'):
 	if cv2.waitKey(5) & 0xFF == ord('q'):
 		break
 
